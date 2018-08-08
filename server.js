ca4f7d77888ef3ef437b0cea0e53a6f8a399287a
Hyeonseok Oh <hyeonseok.ohh@gmail.com>
2018-08-08 12:28:35 +0900

    Change port from 80 to 3000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = require('./app');
-var port = 80
+var port = 3000
 
 var server = app.listen(port, function(){
     console.log('Multicore_marker RESTful API server started on: ' + port);
